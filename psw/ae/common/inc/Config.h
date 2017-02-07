8f9ad84226aabc8cc2e589c483a6efddccd556b5
Sean Christopherson <sean.j.christopherson@intel.com>
2017-02-07 12:51:44 -0800

    Bind the AESM socket to /var/opt/aesmd/aesm.socket
    Exposing sockets to Docker containers is typically done via bind
    mounts, which does not work for abstract sockets since they don't
    exist in the file system.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>

--- psw/ae/common/inc/Config.h
+++ psw/ae/common/inc/Config.h
@@ -33,7 +33,7 @@
 
 
 #ifndef CONFIG_SOCKET_PATH
-    #define CONFIG_SOCKET_PATH "sgx_aesm_socket_base"
+    #define CONFIG_SOCKET_PATH "/var/opt/aesmd/aesm.socket"
 #endif /* CONFIG_SOCKET_PATH */
 
 
